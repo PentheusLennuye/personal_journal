5bfb6bf837cfe5373a16c9768f5959d0d74bf792
PentheusLennuye <gmc@cummings-online.ca>
2024-11-06 17:18:55 -0500

    Converted project to postgres on docker

---
+++ development/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# File: /opt/entrypoint.sh
+# Set up the .pg files from env
+# Initial migrations and, if any tables have changed, make migration
+# Start the dev service
+
+cat <<EOPGS >> $HOME/.pg_service.conf
+[personal_journal]
+host=$DB_HOST
+user=$DB_USER
+dbname=$DB_NAME
+port=$DB_PORT
+EOPGS
+
+cat <<EOPGP >> $HOME/.pj_pgpass
+$DB_HOST:$DB_PORT:$DB_USER:$DB_PASS
+EOPGP
+
+./manage.py makemigrations
+./manage.py migrate
+./manage.py runserver
